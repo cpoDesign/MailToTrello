1789fd4c60dd43792d3a037939a1da6e8ec8e6e4
cpoDesign <pavel.svarc@cpodesign.com>
2015-07-26 22:06:32 +0100

    Cleanup and added new entity for email

---
+++ Tester/Email.Entities/EmailMessage.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Email.Entities
+{
+    public class EmailMessage
+    {
+        public int Counter { get; private set; }
+
+        public EmailMessage(int counter)
+        {
+            Counter = counter;
+        }
+    }
+}
