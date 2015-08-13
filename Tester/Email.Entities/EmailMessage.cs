725eeb9af1c4760a62962ff39843d3a275cc6d04
cpoDesign <pavel.svarc@cpodesign.com>
2015-08-13 09:01:30 +0100

    Added new property to the message

--- Tester/Email.Entities/EmailMessage.cs
+++ Tester/Email.Entities/EmailMessage.cs
@@ -4,7 +4,8 @@ namespace Email.Entities
     public class EmailMessage
     {
         public string Subject { get; set; }
-        
+        public string Date { get; set; }
+
         //public int Counter { get; private set; }
 
         //public EmailMessage(int counter)
